8e49e0e1e3b6877dc4bfaa93b12c850b1ce82832
Ibra <jicastellanos@stefanini.com>
2022-09-21 15:48:49 -0300

    hidding key value

--- constants/constants.js
+++ constants/constants.js
@@ -1,7 +1,6 @@
 const constants = {
   urlWeather: 'https://api.openweathermap.org/data/2.5/weather',
   urlforecastFive: 'https://api.openweathermap.org/data/2.5/forecast',
-  keyValue: 'f181042fbdc2be31c4cae8ba716a6338',
   location: 'http://ip-api.com/json/',
   publicIp: 'https://api.ipify.org/?format=json'
 
